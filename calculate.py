4bb429eab1b384f288a15f43f5588f11c9fa7312
natashafuji <natashafredericks24@gmail.com>
2025-07-02 13:16:34 +0200

    FNB app academy- rectangle callculator

---
+++ calculate.py
@@ -0,0 +1,9 @@
+
+def area(lenght, width):
+    #funciton  to calculate the area
+    return lenght * width
+
+def perimeter(length, width):
+    #function to ccalculate perimeter
+     return 2 *(length + width)
+ 
